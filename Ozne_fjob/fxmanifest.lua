c720ec9fe27bc7f4bc6c16eff81d21f0b4200835
Ozne <57328206+Ozneeee@users.noreply.github.com>
2021-03-06 17:47:04 +0100

    Add files via upload

---
+++ Ozne_fjob/fxmanifest.lua
@@ -0,0 +1,6 @@
+fx_version 'adamant'
+game 'gta5'
+version '1'
+
+client_scripts { '@es_extended/locale.lua', 'locales/fr.lua', 'config.lua', 'client/main.lua' }
+server_scripts { '@es_extended/locale.lua', 'locales/fr.lua', 'config.lua', 'server/main.lua' }
